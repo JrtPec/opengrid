e5f2883a8c9f10542d6a2ed559d2d5b246effb80
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-02-11 10:56:29 +0100

    Optional evolution plotting
    New parameter for extract_events(): plot_evolution (default=False).
    True enables evolution plot for notebooks

--- nialm/library/genetic/genetic.py
+++ nialm/library/genetic/genetic.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from IPython import display
 
-def extract_events(problem,generations=100,pop_size=100,elitism=1,breeding_percentage=0.1):
+def extract_events(problem,generations=100,pop_size=100,elitism=1,breeding_percentage=0.1,plot_evolution=False):
 	"""
 		Performs an analysis of switch on-off events using a genetic algorithm, saves the analysis results in seperate files
 
@@ -26,24 +26,27 @@ def extract_events(problem,generations=100,pop_size=100,elitism=1,breeding_perce
 	#build the starting population
 	pop = Population(size=pop_size,problem=problem)
 
-	best_score = [pop.get_score(1)]
-	top_score = [pop.get_score(int(pop_size*breeding_percentage))]
-	mean_score = [pop.get_score(pop_size)]
+	if plot_evolution = True:
+		best_score = [pop.get_score(1)]
+		top_score = [pop.get_score(int(pop_size*breeding_percentage))]
+		mean_score = [pop.get_score(pop_size)]
 
 	#loop for the amount of generations
 	for i in range(0,generations):
 		pop.evolve(elitism=elitism,breeding_percentage=breeding_percentage)
-		best_score.append(pop.get_score(1))
-		top_score.append(pop.get_score(int(pop_size*breeding_percentage)))
-		mean_score.append(pop.get_score(pop_size))
-		display.clear_output(wait=True)
-		plt.plot(best_score, label="best")
-		plt.plot(top_score, label="top")
-		plt.plot(mean_score, label="mean")
-		plt.ylabel('score')
-		plt.xlabel('generations')
-		plt.legend()
-		plt.show()
+		if plot_evolution = True:
+			best_score.append(pop.get_score(1))
+			top_score.append(pop.get_score(int(pop_size*breeding_percentage)))
+			mean_score.append(pop.get_score(pop_size))
+			display.clear_output(wait=True)
+			plt.plot(best_score, label="best")
+			plt.plot(top_score, label="top")
+			plt.plot(mean_score, label="mean")
+			plt.ylabel('score')
+			plt.xlabel('generations')
+			plt.legend()
+			plt.show()
+		
 
 	#find the best individual
 	return pop.get_best()
